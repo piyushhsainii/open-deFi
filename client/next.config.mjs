651fbfdae0448ae788ffe60f6ed18a6de5b253c3
piyushhsainii <sainipiyush8860@gmail.com>
2025-09-30 23:29:34 +0530

    removed config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,30 +1,37 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) {
+//       config.resolve.fallback = {
+//         ...config.resolve.fallback,
+//         fs: false,
+//         net: false,
+//         tls: false,
+//         crypto: require.resolve("crypto-browserify"),
+//         stream: require.resolve("stream-browserify"),
+//         url: require.resolve("url"),
+//         zlib: require.resolve("browserify-zlib"),
+//         http: require.resolve("stream-http"),
+//         https: require.resolve("https-browserify"),
+//         assert: require.resolve("assert"),
+//         os: false,
+//         path: false,
+//         "pino-pretty": false,
+//       };
+//     }
+//     return config;
+//   },
+//   transpilePackages: ["@solana/wallet-adapter-base"],
+// };
+
+// export default nextConfig;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-        crypto: require.resolve("crypto-browserify"),
-        stream: require.resolve("stream-browserify"),
-        url: require.resolve("url"),
-        zlib: require.resolve("browserify-zlib"),
-        http: require.resolve("stream-http"),
-        https: require.resolve("https-browserify"),
-        assert: require.resolve("assert"),
-        os: false,
-        path: false,
-        "pino-pretty": false,
-      };
-    }
-    return config;
-  },
-  transpilePackages: ["@solana/wallet-adapter-base"],
+  reactStrictMode: false,
 };
 
 export default nextConfig;
